d9d3b50648d4a15e0e63c2704d57cccd5fa5dead
captainabap <github@brandeis.de>
2018-08-24 16:11:38 +0200

    Create Listing 1.sql

---
+++ Examples/Chapter1/Listing 1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE namen ( id   INT, 
+        last_name VARCHAR(30), 
+        first_name VARCHAR(30) );
+INSERT INTO namen VALUES ( 1,'Brandeis','Jörg' );
+INSERT INTO namen VALUES ( 2, 'Müller', 'Peter' );
+SELECT * FROM namen;
+DROP TABLE namen;
