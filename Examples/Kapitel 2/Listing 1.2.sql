53e0323b71f44ad54aafaa805ef752b9fe8bcca5
captainabap <github@brandeis.de>
2018-09-19 22:56:24 +0200

    Delete Listing 1.2.sql

--- Examples/Kapitel 2/Listing 1.2.sql
+++
@@ -1,8 +0,0 @@
---Beispiel für mehrere Anweisungen in der SQL-Konsole
-CREATE TABLE namen ( id   INT, 
-        last_name VARCHAR(30), 
-        first_name VARCHAR(30) );
-INSERT INTO namen VALUES ( 1,'Brandeis','Jörg' );
-INSERT INTO namen VALUES ( 2, 'Müller', 'Peter' );
-SELECT * FROM namen;
-DROP TABLE namen;
