3d5d8943f6a08522f366ba8b825bd5a1ea468390
captainabap <github@brandeis.de>
2018-10-03 21:14:57 +0200

    Korrektur: Leerzeichen nach DO hat gefehlt

--- Listings/Kapitel 2/Listing 2.16.sql
+++ Listings/Kapitel 2/Listing 2.16.sql
@@ -1,8 +1,8 @@
 --Anonymer Block mit Parametern
 
-DO(IN  iv_status INT => 1, 
-   OUT ot_aufgaben TABLE ( id INT,
-                           titel NVARCHAR(40) ) => ?)
+DO (IN  iv_status INT => 1, 
+    OUT ot_aufgaben TABLE ( id INT,
+                            titel NVARCHAR(40) ) => ?)
 
 BEGIN
    ot_aufgaben =  SELECT id,
